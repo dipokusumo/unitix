57d2688c545d9b0636a8438e04536e4366d8b38d
Dipo Kusumo <kusumo.dipo.dk@gmail.com>
2024-11-25 22:20:11 +0700

    setup project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl">
+      Hello, Tailwind with React and Vite!
+    </div>
+  );
+}
