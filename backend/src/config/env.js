c2e0669d2e15e0a03e7b693186d10a0ed7751e08
Dipo Kusumo <kusumo.dipo.dk@gmail.com>
2024-11-28 07:22:08 +0700

    setup autentikasi (jwt)

---
+++ backend/src/config/env.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+    port: process.env.PORT,
+    mongodbUri: process.env.MONGODB_URI,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN,
+    unitixEmail: process.env.EMAIL_USER,
+    unitixPassword: process.env.EMAIL_PASS
+};
