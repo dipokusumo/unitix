9b064e79d50f7df292cd9fe3e58ccbca35178d0f
Dimas Adi Syahputra <adisqd77@gmail.com>
2024-11-30 01:30:21 -0500

    Add Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/Dashboard";
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl">
-      Hello, Tailwind with React and Vite!
+    <div>
+      <LoginPage/>
+      <DashboardPage/>
     </div>
   );
 }
